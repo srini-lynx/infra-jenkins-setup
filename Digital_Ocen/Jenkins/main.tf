8101ad04d8a73bd9eacc1eea5f4ad18a1fa477ff
vagrant <vagrant@dev-node039>
2023-06-17 00:18:39 +0000

    main.tf variable.tf updated

--- Digital_Ocen/Jenkins/main.tf
+++ Digital_Ocen/Jenkins/main.tf
@@ -1,19 +1,16 @@
-#https://registry.terraform.io/providers/digitalocean/digitalocean/latest/docs/data-sources/ssh_key
-# Data Source for SSH-key
-data "digitalocean_ssh_key" "example" {
-  name = "example"
+############ https://registry.terraform.io/providers/digitalocean/digitalocean/latest/docs/data-sources/ssh_key################
+################ Data Source for SSH-key
+data "digitalocean_ssh_key" "jenkins-infra" {
+  name = "dev-node039"
 }
 
-#https://registry.terraform.io/providers/digitalocean/digitalocean/latest/docs/resources/droplet
-#Creaet Digital Ocean Droplet
+########### https://registry.terraform.io/providers/digitalocean/digitalocean/latest/docs/resources/droplet###################
+##########  Creaet Digital Ocean Droplet
 
-resource "digitalocean_droplet" "example" {
-  image    = "ubuntu-18-04-x64"
-  name     = "example-1"
-  region   = "nyc2"
-  size     = "s-1vcpu-1gb"
-  ssh_keys = [data.digitalocean_ssh_key.example.id]
+resource "digitalocean_droplet" "jenkins-infra" {
+  image    = var.image 
+  name     = var.server_name 
+  region   = var.server_region 
+  size     = var.image_size 
+  ssh_keys = [data.digitalocean_ssh_key.jenkins-infra.id]
 }
-
-
-
